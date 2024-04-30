c4eddd234e282ecde498f4823e58632df277634a
cube823 <cube823@gmail.com>
2024-04-30 15:55:53 +0900

    🎨 (1325): 백준 1325 효울적인 해킹 수정

--- src/Week2/Day1/1325_efficient_hacking.ts
+++ src/Week2/Day1/1325_efficient_hacking.ts
@@ -1,80 +1,58 @@
-/**
- * 해커 김지민은 잘 알려진 어느 회사를 해킹하려고 한다. 
- * 이 회사는 N개의 컴퓨터로 이루어져 있다. 
- * 김지민은 귀찮기 때문에, 한 번의 해킹으로 여러 개의 컴퓨터를 해킹 할 수 있는 컴퓨터를 해킹하려고 한다.
- * 
- * 이 회사의 컴퓨터는 신뢰하는 관계와, 신뢰하지 않는 관계로 이루어져 있는데, 
- * A가 B를 신뢰하는 경우에는 B를 해킹하면, A도 해킹할 수 있다는 소리다.
-
-이 회사의 컴퓨터의 신뢰하는 관계가 주어졌을 때, 
-한 번에 가장 많은 컴퓨터를 해킹할 수 있는 컴퓨터의 번호를 출력하는 프로그램을 작성하시오.
- */
-
 import { PWD } from '../../pwd'
 
-/**
- * Input
- * 0 < N <= 10^4
- * 0 <= M <= 10^5
- */
-
 const fs = require('fs')
-const input = fs
-  .readFileSync(`${PWD}/src/Week2/Day1/1325_efficient_hacking.txt`)
+const [info, ...input] = fs
+  .readFileSync(
+    process.platform === 'linux' ? '/dev/stdin' : `${PWD}/src/Week2/Day1/1325_efficient_hacking.txt`
+  )
   .toString()
   .trim()
   .split('\n')
+  .map((item: string) => item.split(' ').map((value) => +value))
 
-const [N, M] = input[0].split(' ').map(Number) as number[]
-const graph = Array.from({ length: N + 1 }, () => []) as number[][]
-let cnt = 0
+const [N, M] = info as number[]
 
-for (let i = 1; i <= M; i++) {
-  const [A, B] = input[i].split(' ').map(Number)
-  graph[B].push(A)
-}
+const solution = () => {
+  const graph = Array.from({ length: N + 1 }, () => []) as number[][]
 
-const dfs = (_node: number) => {
-  const visited = Array(N + 1).fill(false)
+  for (let i = 0; i < M; i++) {
+    const [A, B] = input[i] as number[]
+    graph[B].push(A)
+  }
 
-  const hack = (node: number) => {
-    if (visited[node]) return
+  let max = 0
+  let arr: number[] = []
 
-    visited[node] = true
-    const nodes = graph[node]
-    if (!nodes) return
+  const dfs = (n: number) => {
+    const visited = Array(N + 1).fill(false)
+    let count = 1
+    const stack = [n]
 
-    for (const value of nodes.values()) {
-      console.log('value', value)
-      cnt++
-      hack(value)
+    visited[n] = true
+
+    while (stack.length) {
+      const value = stack.pop() as number
+      for (let i = 0; i < graph[value].length; i++) {
+        if (!visited[graph[value][i]]) {
+          count++
+          stack.push(graph[value][i])
+        }
+      }
+    }
+
+    if (max > count) return
+    else if (max === count) arr.push(n)
+    else {
+      max = count
+      arr = [n]
     }
   }
 
-  hack(_node)
-
-  return visited.filter((visit) => visit).length
-}
-
-let max = 0
-const arr: number[] = [0]
-
-for (let i = 1; i <= N; i++) {
-  if (!graph[i].length) continue
-  const visited = dfs(i)
-  arr.push(visited)
-
-  if (visited > max) {
-    max = visited
+  for (let i = 1; i <= N; i++) {
+    dfs(i)
   }
+
+  console.log(arr.join(' '))
 }
 
-const result: number[] = []
-for (let i = 1; i <= N; i++) {
-  if (arr[i] === max) {
-    result.push(i)
-  }
-}
-
-console.log(cnt)
-console.log(result.join(' '))
+solution()
