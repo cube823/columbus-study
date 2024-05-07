497aa2ef08d7f51194bf784fc28c61b2c62832f2
cube823 <cube823@gmail.com>
2024-05-07 15:59:10 +0900

    🎨 (7569): 백준 7569 토마토 풀이 수정

--- src/Week3/Day2/7569_tomato.ts
+++ src/Week3/Day2/7569_tomato.ts
@@ -1,5 +1,15 @@
 import { PWD } from '../../pwd'
 
+// 3차원 문제
+// MxN grid 가 H 개 반복되는 2차원 그리드로 푸는게 단순할 것으로 예상
+// H 개가 없는 tomato 문제를 풀었던 적이 있다.
+// 이때랑 마찬가지로 (아마도) answers map 을 활용하는 방식을 택하면 될 것 같다.
+// bfs + answersMap
+
+// 1인 좌표를 담은 배열을 만든다.
+// 해당 배열을 queue에 넣어 하루씩 익도록 만든다.
+// 배열 내 0이 존재하지 않으면 가장 큰 수를, 0이 존재하면 -1을 리턴한다.
+
 const fs = require('fs').readFileSync(`${PWD}/src/Week3/Day2/7569_tomato2.txt`)
 const input = fs
   .toString()
@@ -11,43 +21,50 @@ const queue = [] as number[][]
 const stack = [] as number[][]
 
 const [M, N, H] = input.shift().map(Number) as number[]
+let cnt = 0
 
-const dx = [1, -1, 0, 0, N, -N]
+const dx = [1, -1, 0, 0, 0, 0]
 const dy = [0, 0, 1, -1, 0, 0]
+const dz = [0, 0, 0, 0, 1, -1]
 
-const tomatoes: number[][] = input.map((s: string[]) => s.map((el) => Number(el)))
+const grid: number[][] = input.map((s: string[]) => s.map((el) => Number(el)))
 
-// 3차원 문제
-// MxN grid 가 H 개 반복되는 2차원 그리드로 푸는게 단순할 것으로 예상
-// H 개가 없는 tomato 문제를 풀었던 적이 있다.
-// 이때랑 마찬가지로 (아마도) answers map 을 활용하는 방식을 택하면 될 것 같다.
-// bfs + answersMap
+const tomatoes: number[][][] = Array.from({ length: H }).map(() => [])
 
-// 1인 좌표를 담은 배열을 만든다.
-// 해당 배열을 queue에 넣어 하루씩 익도록 만든다.
-// 배열 내 0이 존재하지 않으면 가장 큰 수를, 0이 존재하면 -1을 리턴한다.
+grid.forEach((list, index) => {
+  tomatoes[Math.floor(index / N)].push(list)
+})
 
-const visited = Array.from({ length: N * H }).map(() => Array.from({ length: M }).map(() => false))
+const visited = tomatoes.map((list) => list.map((item) => item.map(() => false)))
+const answers = tomatoes.map((list) => list.map((item) => item.map(() => 0)))
+
+let freshTomatoes = 0
 
 const solution = () => {
-  let cnt = 0
-
-  for (let x = 0; x < N * H; x++) {
-    for (let y = 0; y < M; y++) {
-      if (tomatoes[x][y] === 1) queue.push([x, y])
+  for (let d = 0; d < H; d++) {
+    for (let x = 0; x < N; x++) {
+      for (let y = 0; y < M; y++) {
+        if (tomatoes[d][x][y] === 1) queue.push([d, x, y])
+        else if (tomatoes[d][x][y] === 0) freshTomatoes++
+      }
     }
   }
 
   while (queue.length) {
-    const [x, y] = queue.pop() as number[]
+    const [d, x, y] = queue.pop() as number[]
 
     for (let i = 0; i < 6; i++) {
       const xx = x + dx[i]
       const yy = y + dy[i]
+      const dd = d + dz[i]
 
-      if (xx < 0 || yy < 0 || xx > N * H - 1 || yy > M - 1) continue
-      if (tomatoes[xx][yy] === 0 && !visited[xx][yy]) stack.push([xx, yy])
-      visited[xx][yy] = true
+      if (xx < 0 || yy < 0 || dd < 0 || xx > N - 1 || yy > M - 1 || dd > H - 1) continue
+      if (tomatoes[dd][xx][yy] === 0 && !visited[dd][xx][yy]) {
+        stack.push([dd, xx, yy])
+        answers[dd][xx][yy] = answers[d][x][y] + 1
+        freshTomatoes--
+      }
+      visited[dd][xx][yy] = true
     }
 
     if (!queue.length && stack.length) {
@@ -56,13 +73,7 @@ const solution = () => {
     }
   }
 
-  for (let i = 0; i < N * H; i++) {
-    for (let j = 0; j < M; j++) {
-      if (visited[i][j] === false) return -1
-    }
-  }
-
-  return cnt
+  return freshTomatoes ? -1 : cnt
 }
 
 console.log(solution())
