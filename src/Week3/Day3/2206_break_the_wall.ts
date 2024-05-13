7d57e438783b92fd3ddcef46397ab73d5aa20f2f
cube823 <cube823@gmail.com>
2024-05-13 10:27:45 +0900

    🎨 (2206): 백준 2206 벽부수고 이동하기 풀이

--- src/Week3/Day3/2206_break_the_wall.ts
+++ src/Week3/Day3/2206_break_the_wall.ts
@@ -1,5 +1,43 @@
-import { Queue } from '../../Common/queue'
 import { PWD } from '../../pwd'
+
+class Node {
+  item: number[]
+  next: Node | null
+  constructor(item: number[]) {
+    this.item = item
+    this.next = null
+  }
+}
+
+class Queue {
+  head: Node | null
+  tail: Node | null
+  length: number
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  push(item: number[]) {
+    const node = new Node(item)
+    if (this.head == null) {
+      this.head = node
+    } else {
+      if (this.tail) this.tail.next = node
+    }
+
+    this.tail = node
+    this.length += 1
+  }
+
+  pop() {
+    const popItem = this.head
+    if (this.head) this.head = this.head.next
+    this.length -= 1
+    return popItem?.item
+  }
+}
 
 const fs = require('fs')
 const [info, ...input] = fs
@@ -11,41 +49,46 @@ const [info, ...input] = fs
 const [N, M] = info.split(' ').map(Number) as number[]
 const grid = input.map((str: string) => str.split('').map(Number)) as number[][]
 
-const visited = Array.from({ length: N }).map(() => Array.from({ length: M }).map(() => false))
+const answerMap = Array.from({ length: N }).map(() => Array.from({ length: M }).map(() => [0, 0]))
 
 const dx = [1, -1, 0, 0]
 const dy = [0, 0, 1, -1]
 
 const solution = () => {
   const queue = new Queue()
-  queue.push([0, 0, 0, 1]) // x, y, flag, cnt
+  queue.push([0, 0, 0, 1, -1]) // x, y, flag, cnt
+
+  if (N === 1 && M === 1) return 1
 
   while (queue.length) {
-    const [x, y, flag, cnt] = queue.pop() as number[]
+    const [x, y, flag] = queue.pop() as number[]
+
+    if (x === N - 1 && y === M - 1) return answerMap[x][y][flag]
 
     for (let i = 0; i < 4; i++) {
       const xx = x + dx[i]
       const yy = y + dy[i]
 
       if (xx < 0 || yy < 0 || xx > N - 1 || yy > M - 1) continue
-      if (visited[xx][yy]) continue
+      if (answerMap[xx][yy][flag]) continue
       if (flag && grid[xx][yy] === 1) continue
 
-      visited[xx][yy] = true
-
       if (xx === N - 1 && yy === M - 1) {
-        return cnt + 1
+        answerMap[xx][yy][flag] = answerMap[x][y][flag] + 1
+        return answerMap[xx][yy][flag] + 1
       }
 
-      if (grid[xx][yy] === 1) {
-        queue.push([xx, yy, flag + 1, cnt + 1])
+      if (grid[xx][yy] === 1 && !flag) {
+        answerMap[xx][yy][flag + 1] = answerMap[x][y][flag] + 1
+        queue.push([xx, yy, flag + 1])
       } else {
-        queue.push([xx, yy, flag, cnt + 1])
+        answerMap[xx][yy][flag] = answerMap[x][y][flag] + 1
+        queue.push([xx, yy, flag])
       }
     }
   }
 
-  return -1
+  return answerMap[N - 1][M - 1][0] || answerMap[N - 1][M - 1][1] || -1
 }
 
 console.log(solution())
